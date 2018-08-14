7c36d6f2315f6f3f024f20062a53ad7372d5f9c8
Patrick McCombs <pa.mccombs1@gmail.com>
2018-08-14 03:33:54 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
   #your code here
   attr_accessible :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
-  def initialize(key)
+  def initialize(attributes)
     key.send= value
     
   end
